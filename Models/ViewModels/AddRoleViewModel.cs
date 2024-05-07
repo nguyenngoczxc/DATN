6ea30979ca1b7b789fb8880d4925891d7bdc0758
ngoc <ngoc288288@gmail.com>
2024-05-07 09:36:55 +0700

    account

---
+++ Models/ViewModels/AddRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TTTN3.Models.ViewModels
+{
+    public class AddRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<string> RoleNames { get; set; }
+        public string SelectedRoleName { get; set; }
+    }
+
+}
---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TTTN3.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Cannot be left blank")]
+        public string user_Name { get; set; }
+        [DataType(DataType.Password), Required(ErrorMessage = "Cannot be left blank")]
+        public string password { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
